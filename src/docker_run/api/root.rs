82c03711ffa9cfb8896dae777ba753ef36d14742
Petter Rasmussen <petter@hask.no>
2020-10-24 13:23:17 +0200

    Chang order of fields in struct

--- src/docker_run/api/root.rs
+++ src/docker_run/api/root.rs
@@ -9,16 +9,16 @@ const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 #[derive(Debug, serde::Serialize)]
 struct Response {
     name: String,
-    description: String,
     version: String,
+    description: String,
 }
 
 pub fn handle(_: &config::Config, _: &mut tiny_http::Request) -> Result<Vec<u8>, api::Error> {
 
     let response = Response{
         name: "docker-run".to_string(),
-        description: "Api for running code in transient docker containers".to_string(),
         version: VERSION.unwrap_or("unknown").to_string(),
+        description: "Api for running code in transient docker containers".to_string(),
     };
 
     serde_json::to_vec_pretty(&response).map_err(|err| {
